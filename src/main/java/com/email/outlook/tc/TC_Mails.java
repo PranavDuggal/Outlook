24c7355ceae2e847cfaec957262d7ccda50fb86f
PranavDuggal <pranavduggal1999@yahoo.com>
2022-01-31 22:30:07 +0530

    Updated

--- src/main/java/com/email/outlook/tc/TC_Mails.java
+++ src/main/java/com/email/outlook/tc/TC_Mails.java
@@ -2,7 +2,7 @@ package com.email.outlook.tc;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Scanner;
+
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -19,7 +19,7 @@ public class TC_Mails {
 	@Parameters({"browser","url"})
 	public void unreadmails(String browser,String url) throws InterruptedException, IOException{
 		
-			Scanner sc = new Scanner(System.in);
+			
 			WebDriver driver = BrowserManager.getDriver(browser,url);
 			PO_Mails obj = PageFactory.initElements(driver,PO_Mails.class);
 			obj.Home();
